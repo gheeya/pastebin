709d312981650af96aacf9e9975ddc00b8c37609
gheeya <thegheeya@gmail.com>
2026-01-29 00:53:59 +0530

    Added home page

---
+++ frontend/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Header() {
+  return (
+    <header className="primary-header px-5 py-2.5 w-full h-20">
+      <h2 className="text-4xl font-bold ">Paste.io</h2>
+    </header>
+  );
+}
+
+export default Header;
